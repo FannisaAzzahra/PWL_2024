7fec063934a35721c406886114e93874be3ca4d0
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2024-09-02 09:47:48 +0700

    Pertemuan 2 | Membuat 4 route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2241760102, Nama: Fannisa Azzahra'; 
 });
